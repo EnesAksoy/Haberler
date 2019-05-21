dc8ebc720d8494efb6ab9b41b1b16d75f08f0cac
Enes Aksoy <enesaksoy1168@gmail.com>
2019-05-21 05:37:17 +0300

    minor fix

--- Haberler/ScrollVC.swift
+++ Haberler/ScrollVC.swift
@@ -8,14 +8,20 @@
 
 import UIKit
 
+
 class ScrollVC: UIView {
 
     @IBOutlet weak var imageView: UIImageView!
     @IBOutlet weak var titleLabel: UILabel!
     @IBOutlet weak var explanationLabel: UILabel!
-   
+    
+    var news = [String] ()
     
     
-
-
+    
+    
+    
+    
+    
+    
 }
--- Haberler/ViewController.swift
+++ Haberler/ViewController.swift
@@ -9,7 +9,10 @@
 import UIKit
 import SDWebImage
 
+
 class ViewController: UIViewController,UIScrollViewDelegate {
+    
+    let classss = ScrollVC ()
     
     @IBOutlet weak var scrollView: UIScrollView!
     @IBOutlet weak var pageController: UIPageControl!
@@ -21,6 +24,8 @@ class ViewController: UIViewController,UIScrollViewDelegate {
     
     var slides = [ScrollVC] ()
     
+  
+    
     let apikey : String = "05d0ea935a3a43e6b2aa9b5c018b3675"
     
     override func viewDidLoad() {
@@ -30,21 +35,14 @@ class ViewController: UIViewController,UIScrollViewDelegate {
         scrollView.delegate = self
         rates()
         
-        //        Sayfa belirteçlerinin başlangıç ayarı
-        
         pageController.numberOfPages = 10
-        
         pageController.currentPage = 0
        
         
-       view.addSubview(pageController)
-        
-        
-        //        Dikey kaydırmayı devre dışı bırakmak için
+        view.addSubview(pageController)
         
         self.scrollView.contentSize.height = 1.0
         
-    
     }
     
     func scrollViewDidScroll(_ scrollView: UIScrollView)
@@ -52,55 +50,8 @@ class ViewController: UIViewController,UIScrollViewDelegate {
     {
         let pageIndex = round(scrollView.contentOffset.x/view.frame.width)
         pageController.currentPage = Int(pageIndex)
-        //        Dikey durum
         
-       // let maximumHorizontalOffset: CGFloat = scrollView.contentSize.width - scrollView.frame.width
-        //let currentHorizontalOffset: CGFloat = scrollView.contentOffset.x
-        //        Yatay durum
-        //let maximumVerticalOffset: CGFloat = scrollView.contentSize.height - scrollView.frame.height
-        //let currentVerticalOffset: CGFloat = scrollView.contentOffset.y
-        //        Yatay ve dikey olarak kaçıncı scrollView'de olduğumuzu anlamak için bulunan view / maximum view
-       // let percentageHorizontalOffset: CGFloat = currentHorizontalOffset / maximumHorizontalOffset
-       // let percentageVerticalOffset: CGFloat = currentVerticalOffset / maximumVerticalOffset
-        
-        //let percentOffset: CGPoint = CGPoint(x: percentageHorizontalOffset, y: percentageVerticalOffset)
-        
-        /*if(percentOffset.x > 0 && percentOffset.x <= 0.25)
-        {
-            slides[0].imageView.transform = CGAffineTransform(scaleX: (0.25-percentOffset.x)/0.25, y: (0.25-percentOffset.x)/0.25)
-            
-            slides[1].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.25, y: percentOffset.x/0.25)
-        }
-        else if(percentOffset.x > 0.25 && percentOffset.x <= 0.50)
-        {
-            
-            slides[1].imageView.transform = CGAffineTransform(scaleX: (0.50-percentOffset.x)/0.25, y: (0.50-percentOffset.x)/0.25)
-            
-            slides[2].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.50, y: percentOffset.x/0.50)
-            
-            
-        }
-        else if(percentOffset.x > 0.50 && percentOffset.x <= 0.75)
-        {
-            
-            slides[2].imageView.transform = CGAffineTransform(scaleX: (0.75-percentOffset.x)/0.50, y: (0.75-percentOffset.x)/0.50)
-            
-            slides[3].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/0.75, y: percentOffset.x/0.75)
-            
-            
-        }
-        else if(percentOffset.x > 0.75 && percentOffset.x <= 1)
-        {
-            
-            slides[3].imageView.transform = CGAffineTransform(scaleX: (1-percentOffset.x)/0.75, y: (1-percentOffset.x)/0.75)
-            
-            slides[4].imageView.transform = CGAffineTransform(scaleX: percentOffset.x/1, y: percentOffset.x/1)
-            
-        
-        }*/
     }
-    
-    
     
     func setScrollView(slides: [ScrollVC]) {
         
@@ -126,48 +77,56 @@ class ViewController: UIViewController,UIScrollViewDelegate {
             slide1.imageView.sd_setImage(with: URL(string: urlImages[0] , relativeTo: nil), placeholderImage: nil)
             slide1.titleLabel.text = titleNews[0]
             slide1.explanationLabel.text = explanationNews[0]
+            slide1.news.append(urlNews[0])
             
             
             let slide2:ScrollVC = Bundle.main.loadNibNamed("ScrollView", owner: self, options: nil)?.first as! ScrollVC
             slide2.imageView.sd_setImage(with: URL(string: urlImages[1] , relativeTo: nil), placeholderImage: nil)
             slide2.titleLabel.text = titleNews[1]
             slide2.explanationLabel.text = explanationNews[1]
+            slide2.news.append(urlNews[1])
             
             
             let slide3:ScrollVC = Bundle.main.loadNibNamed("ScrollView", owner: self, options: nil)?.first as! ScrollVC
             slide3.imageView.sd_setImage(with: URL(string: urlImages[2] , relativeTo: nil), placeholderImage: nil)
             slide3.titleLabel.text = titleNews[2]
             slide3.explanationLabel.text = explanationNews[2]
-            
+            slide3.news.append(urlNews[2])
+        
             
             let slide4:ScrollVC = Bundle.main.loadNibNamed("ScrollView", owner: self, options: nil)?.first as! ScrollVC
             slide4.imageView.sd_setImage(with: URL(string: urlImages[3] , relativeTo: nil), placeholderImage: nil)
             slide4.titleLabel.text = titleNews[3]
             slide4.explanationLabel.text = explanationNews[3]
+            slide4.news.append(urlNews[3])
+        
         
             let slide5:ScrollVC = Bundle.main.loadNibNamed("ScrollView", owner: self, options: nil)?.first as! ScrollVC
             slide5.imageView.sd_setImage(with: URL(string: urlImages[4] , relativeTo: nil), placeholderImage: nil)
             slide5.titleLabel.text = titleNews[4]
             slide5.explanationLabel.text = explanationNews[4]
+            slide5.news.append(urlNews[4])
             
             
             let slide6:ScrollVC = Bundle.main.loadNibNamed("ScrollView", owner: self, options: nil)?.first as! ScrollVC
             slide6.imageView.sd_setImage(with: URL(string: urlImages[5] , relativeTo: nil), placeholderImage: nil)
             slide6.titleLabel.text = titleNews[5]
             slide6.explanationLabel.text = explanationNews[5]
-            
+            slide6.news.append(urlNews[5])
             
             
             let slide7:ScrollVC = Bundle.main.loadNibNamed("ScrollView", owner: self, options: nil)?.first as! ScrollVC
             slide7.imageView.sd_setImage(with: URL(string: urlImages[6] , relativeTo: nil), placeholderImage: nil)
             slide7.titleLabel.text = titleNews[6]
             slide7.explanationLabel.text = explanationNews[6]
+            slide7.news.append(urlNews[6])
             
             
             let slide8:ScrollVC = Bundle.main.loadNibNamed("ScrollView", owner: self, options: nil)?.first as! ScrollVC
             slide8.imageView.sd_setImage(with: URL(string: urlImages[7] , relativeTo: nil), placeholderImage: nil)
             slide8.titleLabel.text = titleNews[7]
             slide8.explanationLabel.text = explanationNews[7]
+            slide8.news.append(urlNews[7])
             
             
             
@@ -175,12 +134,16 @@ class ViewController: UIViewController,UIScrollViewDelegate {
             slide9.imageView.sd_setImage(with: URL(string: urlImages[8] , relativeTo: nil), placeholderImage: nil)
             slide9.titleLabel.text = titleNews[8]
             slide9.explanationLabel.text = explanationNews[8]
+            slide9.news.append(urlNews[8])
             
             
             let slide10:ScrollVC = Bundle.main.loadNibNamed("ScrollView", owner: self, options: nil)?.first as! ScrollVC
             slide10.imageView.sd_setImage(with: URL(string: urlImages[9] , relativeTo: nil), placeholderImage: nil)
             slide10.titleLabel.text = titleNews[9]
             slide10.explanationLabel.text = explanationNews[9]
+            slide10.news.append(urlNews[9])
+        
+        
             
             return [slide1, slide2, slide3, slide4, slide5, slide6, slide7, slide8, slide9, slide10]
             
